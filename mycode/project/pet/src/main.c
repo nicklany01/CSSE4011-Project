8944fc72f1a5fdde81f3058331e8d2a1ce4869bf
Nick Lany <nicklany01@gmail.com>
2025-05-24 14:27:00 +1000

    Added SD Card Code

--- mycode/project/pet/src/main.c
+++ mycode/project/pet/src/main.c
@@ -1,150 +1,227 @@
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/display.h>
-#include <zephyr/drivers/gpio.h>
-#include <lvgl.h>
-#include <stdio.h>
-#include <string.h>
 #include <zephyr/kernel.h>
-#include <lvgl_input_device.h>
-
-#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/device.h>
+#include <zephyr/storage/disk_access.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(app);
+#include <zephyr/fs/fs.h>
 
-static uint32_t count;
+#if defined(CONFIG_FAT_FILESYSTEM_ELM)
 
-#ifdef CONFIG_RESET_COUNTER_SW0
-static struct gpio_dt_spec button_gpio = GPIO_DT_SPEC_GET_OR(
-		DT_ALIAS(sw0), gpios, {0});
-static struct gpio_callback button_callback;
+#include <ff.h>
 
-static void button_isr_callback(const struct device *port,
-				struct gpio_callback *cb,
-				uint32_t pins)
+/*
+ *  Note the fatfs library is able to mount only strings inside _VOLUME_STRS
+ *  in ffconf.h
+ */
+#if defined(CONFIG_DISK_DRIVER_MMC)
+#define DISK_DRIVE_NAME "SD2"
+#else
+#define DISK_DRIVE_NAME "SD"
+#endif
+
+#define DISK_MOUNT_PT "/SD:"
+
+static FATFS fat_fs;
+/* mounting info */
+static struct fs_mount_t mp = {
+	.type = FS_FATFS,
+	.fs_data = &fat_fs,
+};
+
+#elif defined(CONFIG_FILE_SYSTEM_EXT2)
+
+#include <zephyr/fs/ext2.h>
+
+#define DISK_DRIVE_NAME "SD"
+#define DISK_MOUNT_PT "/ext"
+
+static struct fs_mount_t mp = {
+	.type = FS_EXT2,
+	.flags = FS_MOUNT_FLAG_NO_FORMAT,
+	.storage_dev = (void *)DISK_DRIVE_NAME,
+	.mnt_point = "/ext",
+};
+
+#endif
+
+#if defined(CONFIG_FAT_FILESYSTEM_ELM)
+#define FS_RET_OK FR_OK
+#else
+#define FS_RET_OK 0
+#endif
+
+LOG_MODULE_REGISTER(main);
+
+#define MAX_PATH 128
+#define SOME_FILE_NAME "some.dat"
+#define SOME_DIR_NAME "some"
+#define SOME_REQUIRED_LEN MAX(sizeof(SOME_FILE_NAME), sizeof(SOME_DIR_NAME))
+
+static int lsdir(const char *path);
+#ifdef CONFIG_FS_SAMPLE_CREATE_SOME_ENTRIES
+static bool create_some_entries(const char *base_path)
 {
-	ARG_UNUSED(port);
-	ARG_UNUSED(cb);
-	ARG_UNUSED(pins);
+	char path[MAX_PATH];
+	struct fs_file_t file;
+	int base = strlen(base_path);
 
-	count = 0;
+	fs_file_t_init(&file);
+
+	if (base >= (sizeof(path) - SOME_REQUIRED_LEN)) {
+		LOG_ERR("Not enough concatenation buffer to create file paths");
+		return false;
+	}
+
+	LOG_INF("Creating some dir entries in %s", base_path);
+	strncpy(path, base_path, sizeof(path));
+
+	path[base++] = '/';
+	path[base] = 0;
+	strcat(&path[base], SOME_FILE_NAME);
+
+	if (fs_open(&file, path, FS_O_CREATE) != 0) {
+		LOG_ERR("Failed to create file %s", path);
+		return false;
+	}
+	fs_close(&file);
+
+	path[base] = 0;
+	strcat(&path[base], SOME_DIR_NAME);
+
+	if (fs_mkdir(path) != 0) {
+		LOG_ERR("Failed to create dir %s", path);
+		/* If code gets here, it has at least successes to create the
+		 * file so allow function to return true.
+		 */
+	}
+	return true;
 }
-#endif /* CONFIG_RESET_COUNTER_SW0 */
+#endif
 
-#ifdef CONFIG_LV_Z_ENCODER_INPUT
-static const struct device *lvgl_encoder =
-	DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_encoder_input));
-#endif /* CONFIG_LV_Z_ENCODER_INPUT */
-
-#ifdef CONFIG_LV_Z_KEYPAD_INPUT
-static const struct device *lvgl_keypad =
-	DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_keypad_input));
-#endif /* CONFIG_LV_Z_KEYPAD_INPUT */
-
-static void lv_btn_click_callback(lv_event_t *e)
-{
-	ARG_UNUSED(e);
-
-	count = 0;
-}
+static const char *disk_mount_pt = DISK_MOUNT_PT;
 
 int main(void)
 {
-	char count_str[11] = {0};
-	const struct device *display_dev;
-	lv_obj_t *hello_world_label;
-	lv_obj_t *count_label;
+	/* raw disk i/o */
+	do {
+		static const char *disk_pdrv = DISK_DRIVE_NAME;
+		uint64_t memory_size_mb;
+		uint32_t block_count;
+		uint32_t block_size;
 
-	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
-	if (!device_is_ready(display_dev)) {
-		LOG_ERR("Device not ready, aborting test");
-		return 0;
-	}
-
-#ifdef CONFIG_RESET_COUNTER_SW0
-	if (gpio_is_ready_dt(&button_gpio)) {
-		int err;
-
-		err = gpio_pin_configure_dt(&button_gpio, GPIO_INPUT);
-		if (err) {
-			LOG_ERR("failed to configure button gpio: %d", err);
-			return 0;
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_CTRL_INIT, NULL) != 0) {
+			LOG_ERR("Storage init ERROR!");
+			break;
 		}
 
-		gpio_init_callback(&button_callback, button_isr_callback,
-				   BIT(button_gpio.pin));
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+			LOG_ERR("Unable to get sector count");
+			break;
+		}
+		LOG_INF("Block count %u", block_count);
 
-		err = gpio_add_callback(button_gpio.port, &button_callback);
-		if (err) {
-			LOG_ERR("failed to add button callback: %d", err);
-			return 0;
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+			LOG_ERR("Unable to get sector size");
+			break;
+		}
+		printk("Sector size %u\n", block_size);
+
+		memory_size_mb = (uint64_t)block_count * block_size;
+		printk("Memory Size(MB) %u\n", (uint32_t)(memory_size_mb >> 20));
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_CTRL_DEINIT, NULL) != 0) {
+			LOG_ERR("Storage deinit ERROR!");
+			break;
+		}
+	} while (0);
+
+	mp.mnt_point = disk_mount_pt;
+
+	int res = fs_mount(&mp);
+
+	if (res == FS_RET_OK) {
+		printk("Disk mounted.\n");
+		/* Try to unmount and remount the disk */
+		res = fs_unmount(&mp);
+		if (res != FS_RET_OK) {
+			printk("Error unmounting disk\n");
+			return res;
+		}
+		res = fs_mount(&mp);
+		if (res != FS_RET_OK) {
+			printk("Error remounting disk\n");
+			return res;
 		}
 
-		err = gpio_pin_interrupt_configure_dt(&button_gpio,
-						      GPIO_INT_EDGE_TO_ACTIVE);
-		if (err) {
-			LOG_ERR("failed to enable button callback: %d", err);
-			return 0;
+		if (lsdir(disk_mount_pt) == 0) {
+#ifdef CONFIG_FS_SAMPLE_CREATE_SOME_ENTRIES
+			if (create_some_entries(disk_mount_pt)) {
+				lsdir(disk_mount_pt);
+			}
+#endif
 		}
-	}
-#endif /* CONFIG_RESET_COUNTER_SW0 */
-
-#ifdef CONFIG_LV_Z_ENCODER_INPUT
-	lv_obj_t *arc;
-	lv_group_t *arc_group;
-
-	arc = lv_arc_create(lv_screen_active());
-	lv_obj_align(arc, LV_ALIGN_CENTER, 0, -15);
-	lv_obj_set_size(arc, 150, 150);
-
-	arc_group = lv_group_create();
-	lv_group_add_obj(arc_group, arc);
-	lv_indev_set_group(lvgl_input_get_indev(lvgl_encoder), arc_group);
-#endif /* CONFIG_LV_Z_ENCODER_INPUT */
-
-#ifdef CONFIG_LV_Z_KEYPAD_INPUT
-	lv_obj_t *btn_matrix;
-	lv_group_t *btn_matrix_group;
-	static const char *const btnm_map[] = {"1", "2", "3", "4", ""};
-
-	btn_matrix = lv_buttonmatrix_create(lv_screen_active());
-	lv_obj_align(btn_matrix, LV_ALIGN_CENTER, 0, 70);
-	lv_buttonmatrix_set_map(btn_matrix, (const char **)btnm_map);
-	lv_obj_set_size(btn_matrix, 100, 50);
-
-	btn_matrix_group = lv_group_create();
-	lv_group_add_obj(btn_matrix_group, btn_matrix);
-	lv_indev_set_group(lvgl_input_get_indev(lvgl_keypad), btn_matrix_group);
-#endif /* CONFIG_LV_Z_KEYPAD_INPUT */
-
-	if (IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
-		lv_obj_t *hello_world_button;
-
-		hello_world_button = lv_button_create(lv_screen_active());
-		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, -15);
-		lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED,
-				    NULL);
-		hello_world_label = lv_label_create(hello_world_button);
 	} else {
-		hello_world_label = lv_label_create(lv_screen_active());
+		printk("Error mounting disk.\n");
 	}
 
-	lv_label_set_text(hello_world_label, "Hello world!");
-	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
-
-	count_label = lv_label_create(lv_screen_active());
-	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
-
-	lv_timer_handler();
-	display_blanking_off(display_dev);
+	fs_unmount(&mp);
 
 	while (1) {
-		if ((count % 100) == 0U) {
-			sprintf(count_str, "%d", count/100U);
-			lv_label_set_text(count_label, count_str);
-		}
-		lv_timer_handler();
-		++count;
-		k_sleep(K_MSEC(10));
+		k_sleep(K_MSEC(1000));
 	}
+	return 0;
+}
+
+/* List dir entry by path
+ *
+ * @param path Absolute path to list
+ *
+ * @return Negative errno code on error, number of listed entries on
+ *         success.
+ */
+static int lsdir(const char *path)
+{
+	int res;
+	struct fs_dir_t dirp;
+	static struct fs_dirent entry;
+	int count = 0;
+
+	fs_dir_t_init(&dirp);
+
+	/* Verify fs_opendir() */
+	res = fs_opendir(&dirp, path);
+	if (res) {
+		printk("Error opening dir %s [%d]\n", path, res);
+		return res;
+	}
+
+	printk("\nListing dir %s ...\n", path);
+	for (;;) {
+		/* Verify fs_readdir() */
+		res = fs_readdir(&dirp, &entry);
+
+		/* entry.name[0] == 0 means end-of-dir */
+		if (res || entry.name[0] == 0) {
+			break;
+		}
+
+		if (entry.type == FS_DIR_ENTRY_DIR) {
+			printk("[DIR ] %s\n", entry.name);
+		} else {
+			printk("[FILE] %s (size = %zu)\n",
+				entry.name, entry.size);
+		}
+		count++;
+	}
+
+	/* Verify fs_closedir() */
+	fs_closedir(&dirp);
+	if (res == 0) {
+		res = count;
+	}
+
+	return res;
 }
